c672334c701dd9a7c3492e7aafbc9fc5b0413240
Alejandro Cuevas <alecuevas1996@gmail.com>
2023-11-10 13:27:41 +0100

    card module

---
+++ models/Card.model.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const cardSchema = new Schema(
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: false,
+        unique: true
+      },
+      description: {
+        type: String,
+        trim: true
+      },
+      rarity: {
+        type: String,
+        trim: true,
+      },
+      noSeries: {
+        type: String,
+        trim: true,
+      },
+      language: {
+        type: String,
+        trim: true,
+      },
+      image: {
+        type: String,
+        required: true
+      }
+    },
+    {
+      // this second object adds extra properties: `createdAt` and `updatedAt`    
+      timestamps: true
+    }
+  );
+  
+  const Card = model("Card", cardSchema);
+  
+  module.exports = Card;
